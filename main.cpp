edadd37259e35885c5d05ef3c55b7dfc54d19593
wladaf <wladaf@mail.ru>
2015-11-08 02:25:57 +0300

    adds

--- main.cpp
+++ main.cpp
@@ -3,29 +3,34 @@
 #include <QtWidgets/QApplication>
 #include <QPushButton>
 
+int i, j;
+
 int main(int argv, char **args)
 {
-	int i, j;
-	int n = 5;
-	int size = 30;
 	QApplication app(argv, args);
-
+	int n = 8;
+	int size = 40;
+	int bsize = (n*size - 5)/3 - 5;
 	QPushButton *buttons[n][n];
 
 	QWidget window;
-
-	window.setGeometry(300, 300, size*n, size*n + size + 10);
+	window.setGeometry(300, 300, 10+size*n, 5+size*n + size + 10);
+	window.setFixedSize(window.size());
 	for(i = 0; i < n; i++)
 	{
 		for(j = 0; j < n; j++)
 		{
 			buttons[i][j] = new QPushButton("", &window);
 			QObject::connect(buttons[i][j], SIGNAL(clicked()), qApp, SLOT(quit()));
-			buttons[i][j]->setGeometry(i*size, j*size, size, size);
+			buttons[i][j]->setGeometry(5+i*size, 5+j*size, size, size);
 		}
 	}
-	
-	window.show();
+	QPushButton *newGameButton = new QPushButton("New Game", &window);
+	newGameButton->setGeometry(5, n*size + 10, bsize, size);
 
+	QPushButton *diffButton = new QPushButton("Difficulty", &window);
+	diffButton->setGeometry(5 + 2*n*size/3 + 5, n*size + 10, bsize, size);
+
+	window.show();
 	return app.exec();
 }
